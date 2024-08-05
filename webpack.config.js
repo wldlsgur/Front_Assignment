8fbc6f6d3a5ea2d78978474b6c889abd25d4eb2b
wldlsgur <dhgg321@naver.com>
2024-08-05 23:14:24 +0900

    Chore: webpack 설정

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  name: 'react-beautiful-dnd',
+  context: path.resolve(__dirname, 'src'),
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+    },
+  },
+  entry: './index',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+    }),
+  ],
+  devServer: {
+    client: {
+      overlay: true,
+    },
+    hot: true,
+    open: true,
+    port: 3000,
+  },
+};
